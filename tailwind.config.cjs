637e5d9cd98d07c40cc20f614a7494405ae39874
Orr Shalev <shalev.orr@gmail.com>
2022-08-30 00:32:12 -0400

    start

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      'sans': ['ui-sans-serif', 'system-ui', ],
+      'serif': ['ui-serif', 'Georgia', ],
+      'mono': ['ui-monospace', 'SFMono-Regular', ],
+      'small': ['Nunito Sans', 'sans-serif', ],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
